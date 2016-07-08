b793f795c32a1608de6ecaed45d247300c080c9a
unknown <kris.t.tsoi@gmail.com>
2016-07-08 14:05:57 +1200

    start method

---
+++ src/FourLetter.java
@@ -0,0 +1,18 @@
+
+public class FourLetter {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	String[] fourLettersInCommon(String source, String[] dictionary){
+		
+		
+		return null;
+		
+		
+	}
+	
+
+}
